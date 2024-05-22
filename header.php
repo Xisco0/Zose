2eee1480317d69bc443b24ea52ec0cca60f475c7
Xisco0 <francisbamirin45@gmail.com>
2024-05-22 08:18:31 -0700

    first commit

---
+++ header.php
@@ -0,0 +1,47 @@
+<header>
+		<div class="top-div">
+			<div class="inner-div">
+				<div><p>if you can envision it, than we can build it</p></div>
+				<div class="icon-div">
+					<a href="#" title="facebook">
+						<i class ="bi-facebook"></i></a>
+						<a href="#" title="twitter">
+						<i class ="bi-twitter"></i></a>
+						<a href="#" title="instagram">
+						<i class ="bi-instagram"></i></a>
+						<a href="#" title="youtube">
+						<i class ="bi-youtube"></i></a>
+				</div>
+			</div>
+		</div>
+
+
+		<div class="bottom-div">
+			<div class="inner-div">
+				<div><img src="all-images/Images/zose-logo.png" alt=""></div>
+				<nav>
+					<ul>
+						<li>Home</li>
+						<li>Shop</li>
+						<li>Pages</li>
+						<li>About</li>
+						<li>Blog</li>
+						<li>Contact</li>
+					</ul>
+				</nav>
+
+				<div class="icon-div">
+					<a href="#" title="search">
+						<i class="bi-search"></i></a>
+
+						<a href="#" title="cart">
+						<i class="bi-bag">
+							<div class="count">0</div>
+						</i></a>
+
+						<a href="#" title="settings">
+						<i class="bi-gear"></i></a>
+				</div>
+			</div>
+		</div>
+	</header>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<?php include "meta.php"?>
+	<title>ZOSE</title>
+	
+</head>
+<body>
+	<?php include"header.php"?>
+
+	<div class="slide-div">
+		<div class="slide-div-in">
+
+			<div class="control-div">
+				<h2><span> CCTV SYSTEMS </span><br/>& ACCESS CONTROL</h2>
+				<p>Suspendisse tincidunt ornare sem, at venenatis<br/> lorem tempor vel. 
+					Sed feugiat sit amet nisi non<br/> dignissim. Duis egestas augue at nisi
+					 pharetra<br/> porta enenatis lorem tempor.</p>
+
+					 <button class="btn">Order Now <i class="bi-arrow-right"></i></button>
+			</div>
+
+
+			<div class="img-div">
+				<img src="./all-images/Images/camera.png" alt="camera">
+			</div>
+
+			
+		</div>
+	</div>
+
+
+
+	<footer></footer>
+</body>
+</html>
